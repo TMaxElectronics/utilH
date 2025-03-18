023aedb3be162655c0fafd7f799ce10b4d2ad244
TMaxElectronics <60486146+TMaxElectronics@users.noreply.github.com>
2025-03-18 22:11:00 +0100

    Added some bit&byte operations

--- include/util.h
+++ include/util.h
@@ -73,4 +73,35 @@ int32_t qSin(int32_t x);
 uint32_t isAsciiNumber(char c);
 uint32_t isAsciiSpecialCharacter(char c);
 
+
+
+
+
+
+
+
+//an array containing bit masks with the correct offset
+extern const uint32_t util_bitMasks[];
+
+typedef enum {LITTLE_ENDIAN, BIG_ENDIAN} Endiannes_t;
+
+
+//gets a 16bit word from an array of bytes
+//WARNING: user must make sure that offset+1 bytes are available in the data buffer!
+uint16_t get16BitWord(uint8_t * data, uint32_t offset, Endiannes_t endian);
+//sets a 16bit word in an array of bytes
+//WARNING: user must make sure that the data buffer is large enough!
+uint16_t set16BitWord(uint8_t * data, uint32_t offset, Endiannes_t endian, uint32_t value);
+
+//get a bit out of a number of bits in a byte array
+//WARNING: user must make sure that enough bits are in the data buffer!
+uint32_t getBit(uint8_t * data, uint32_t bitNumber);
+//set a bit in a byte array
+//WARNING: user must make sure that the data buffer is large enough to even have the bit in question!
+void setBit(uint8_t * data, uint32_t bitNumber, uint32_t value);
+
+//divide rounding up the result
+//WARNING: x+y must by <= INT32_MAX
+int32_t ceil_div(int32_t x, int32_t y);
+
 #endif
